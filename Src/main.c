bb145f0ff2fca37dfbe602c4b9d01cb309b75d41
Farhang <f.g.alidoust@gmail.com>
2019-09-07 12:31:34 +0430

    Toggle LED to show receive

--- Src/main.c
+++ Src/main.c
@@ -87,6 +87,7 @@ void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
 		HAL_SPI_Receive_IT(&hspi2,data_rx,2);
 	
 	CDC_Transmit_FS(data_rx,2);
+	HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
 }
 
 /* USER CODE END 0 */
@@ -248,6 +249,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
   /*Configure GPIO pins : PE4 PE3 PE2 PE1 
                            PE0 PE5 PE6 PE8 
                            PE9 PE11 PE14 PE7 
@@ -331,13 +335,13 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOJ, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PI8 PI4 PI5 PI7 
-                           PI10 PI6 PI1 PI12 
-                           PI9 PI0 PI11 PI13 
-                           PI15 PI14 */
+                           PI10 PI6 PI12 PI9 
+                           PI0 PI11 PI13 PI15 
+                           PI14 */
   GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_7 
-                          |GPIO_PIN_10|GPIO_PIN_6|GPIO_PIN_1|GPIO_PIN_12 
-                          |GPIO_PIN_9|GPIO_PIN_0|GPIO_PIN_11|GPIO_PIN_13 
-                          |GPIO_PIN_15|GPIO_PIN_14;
+                          |GPIO_PIN_10|GPIO_PIN_6|GPIO_PIN_12|GPIO_PIN_9 
+                          |GPIO_PIN_0|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_15 
+                          |GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
@@ -373,6 +377,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
 }
 
